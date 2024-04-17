36c7bd8f0dcf3aeed53e1227c780a5d63d87b067
nic <nicolem.britto@gmail.com>
2024-04-16 22:40:43 -0300

    🐣 Pesquisa com chave encadeada e inner join

---
+++ aula_1604/script_1604.sql
@@ -0,0 +1,23 @@
+/*Encadeamento de chaves relacionais*/
+
+select
+		emp.nome as empregado, 
+		depto.nome as departamento 
+from
+	empregado emp, departamento depto 
+where 
+	depto.cod_depto = emp.cod_depto;
+    
+/*Join Inner e Left*/
+
+select 
+	emp.nome as empregado,
+    count(dep.cod_dep) as dependentes
+from
+	empregado emp
+inner join
+	dependente dep
+where
+	emp.cod_emp = dep.cod_emp
+group by
+	emp.nome;
